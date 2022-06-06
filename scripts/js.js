0949b29099b49911c63e9c822879ca3b597ae893
system205 <arsenmutalapov@gmail.com>
2022-06-06 17:49:16 +0300

    added quotes

--- scripts/js.js
+++ scripts/js.js
@@ -1,2 +1,31 @@
 
+let quote = author = '&copy';
+const token = '35174d0b64c5690e2e2fce564c92be0b';
+
+
+onload = async () =>{
+    document.getElementById('quote-slider').style.display = "none";
+    await setQuote();
+    document.getElementById('quote-slider').style.display = 'flex';
+}
+
+async function setQuote(){
+    await fetch('https://favqs.com/api/quotes', {
+        method: 'GET',
+        headers:{
+            'Authorization': `Token token="${token}"`
+        }
+    })
+    .then( x => x.json())
+    .then(x => {quote = x.quotes[0].body; author = x.quotes[0].author;})
+    .catch( () => {quote = '500'; author = 'Server';});
+
+    document.getElementById('quote').innerText = '\u00AB'+quote+'\u00BB';
+    document.getElementById('quote-author').innerText = '\u00A9' + author;
+    
+}
+
+document.getElementById('next-quote-btn').addEventListener('click', setQuote);
+
+
 
