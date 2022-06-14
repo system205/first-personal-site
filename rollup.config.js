786f24faa62a77f7439ca68bc16b1be99c4d02a5
system205 <arsenmutalapov@gmail.com>
2022-06-14 15:40:45 +0300

    add terser and typescript plugins to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,11 @@
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+
 export default {
     input: './src/main.js',
     output: { file: './scripts/bundle.js' },
+    plugins: [
+        resolve({ browser: true }),
+        terser(),
+    ],
 };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 console.log('hello world!');
+
+
+let hello = '1';
+console.log('hello' + hello)
